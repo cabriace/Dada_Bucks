fea418855f2a281b231ddfeb92a36c1684f9d495
Cesar Cabriales <165403890+cabriace@users.noreply.github.com>
2026-02-06 09:22:47 -0700

    Delete src directory

--- src/pages/auth/index.ts
+++
@@ -1,4 +0,0 @@
-export { AuthPage } from './AuthPage';
-export { RoleSelectorPage } from './RoleSelectorPage';
-export { ParentSignupPage } from './ParentSignupPage';
-export { ChildSignupPage } from './ChildSignupPage';
